f4e196a31e20cf0f821a8001d5e41c84fd2f7bcc
keclements <kristin.clements@gmail.com>
2019-03-01 14:50:37 -0500

    small_elevation array created from function

--- pathfinder.py
+++ pathfinder.py
@@ -1,30 +1,36 @@
-#def get_2d_array(text_file):
-	#with open(text_file) as file:
-		#elevation_list = [line.split() for line in file]
-		#print(elevation_list)
-		#print(f'The length of each list is {len(elevation_list)}')
-		#print(f'The length is{len(elevation_list[0]) }')
-		#return elevation_list
 
-#get_2d_array('elevation_small.txt')
+
+def get_2d_array(text_file):
+    with open(text_file) as file:
+        elevation_list = [line.split() for line in file]
+        #print(elevation_list)
+        # print(f'The length is{len(elevation_list[0]) }')
+        # return elevation_list
+
+small_elevation_array =  get_2d_array('elevation_small.txt')   # creating variable to function get_2d_array
+
+# Task 2
+# use function get_2d_array calling it by its new variable name small_elevation_list
+
+
+
 
 # try array action on simple example
 
-simple_array = [['1', '2', '3', '4', '5'],
-				['1', '2', '3', '4', '5'],
-				['1', '2', '3', '4', '5'],
-				['9', '8', '7', '6', '5']]
+# simple_array = [['1', '2', '3', '4', '5'],
+# 				['1', '2', '3', '4', '5'],
+# 				['1', '2', '3', '4', '5'],
+# 				['9', '8', '7', '6', '5']]
 
-simple_int_array = [[int(number) for number in row] for row in simple_array]
+# simple_int_array = [[int(number) for number in row] for row in simple_array]
 
-#print(simple_int_array)
+# #print(simple_int_array)
 
-mins = [min(row) for row in simple_int_array]
-smallest_min = min(mins)
-# print(smallest_min)
+# mins = [min(row) for row in simple_int_array]
+# smallest_min = min(mins)
+# # print(smallest_min)
 
-maxs = [max(row) for row in simple_int_array]
-biggest_min = max(maxs)
+# maxs = [max(row) for row in simple_int_array]
+# biggest_min = max(maxs)
 
-print(biggest_min)
-
+# print(biggest_min)
---
+++ text.py
@@ -0,0 +1,48 @@
+filename = elevation_small.txt 
+
+# >>> with open('test.txt', 'w') as file:
+# ...     file.write('Hello World!')
+# >>> with MyOpen('test.txt') as file:
+# ...     print(file.read())
+# Hello World!
+
+# def ()
+
+
+
+# def get_2d_array(text_file):
+# 	with open(text_file) as file:
+# 		elevation_list = [line.split() for line in file]
+# 		print(elevation_list)
+# 		print(f'The length of each list is {len(elevation_list)}')
+# 		print(f'The length is{len(elevation_list[0]) }')
+# 		return elevation_list
+
+# def
+
+# def get_2d_array(text_file):
+# 	#with open(text_file) as file:
+# 		#elevation_list = [line.split() for line in file]
+# 		#print(elevation_list)
+# 		#print(f'The length of each list is {len(elevation_list)}')
+# 		#print(f'The length is{len(elevation_list[0]) }')
+#return elevation_list
+
+
+
+# # class class_list():
+# # 	def
+
+# class MyOpen():
+#     def __init__(self, filename):
+#         self.filename = filename
+#     def __enter__(self):
+#         self.file = open(self.filename)
+#         return self.file
+#     def __exit__(self, *args):
+#         self.file.close()
+
+# 	with open('test.txt', 'w') as file:
+# 	file.write('Hello World!')
+# 	with MyOpen('test.txt') as file:
+# 	print(file.read())
