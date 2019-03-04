ef6caa932a52035f8897770122bb7c8fc88c6b74
keclements <kristin.clements@gmail.com>
2019-03-03 19:16:38 -0500

    folder

---
+++ pathfinder.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+# def get_2d_array(text_file):
+#     with open(text_file) as file:
+#         elevation_list = [line.split() for line in file]
+#         #print(elevation_list)
+#         # print(f'The length is{len(elevation_list[0]) }')
+#         # return elevation_list
+
+
+# small_elevation_array = get_2d_array(
+#     'elevation_small.txt')  # creating variable to function get_2d_array
