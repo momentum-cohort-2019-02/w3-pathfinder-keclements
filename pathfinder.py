91fe758d905b8e12aebc35b09c1ee6a6d4689fca
KE Clements <31896853+keclements@users.noreply.github.com>
2019-04-08 10:19:08 -0400

    map draw

--- pathfinder.py
+++ pathfinder.py
@@ -1,13 +1,28 @@
-from PIL import Image, ImageColor
+from PIL import image, ImageDraw
 
-im = Image.new('RGBA', (600, 600))
-im.getpixel((0,0))
-for x in range(600):
-    for y in range(300):
-        im.putpixel((x,y), (210,210,210))
-# for x in range(100):
-#     for y in range(600, 600):
+class Map:
 
-        im.putpixel((x,y), ImageColor.getcolor('darkgray', 'RGBA'))
-        im.save('putPixel.png')
-#im.save('whiteImage.png')
+	def __init__(self, filename):
+		with open(filename) as file:
+			self.elevations = [[int(e) for e in line.split()] for line in filename
+			self.max_elevation = max([max(row) for row in self.elevations])
+            self.min_elevation = min([min(row) for row in self.elevations])
+
+	def elevation_coordinates(self, x, y):
+		return self.elevations[y][x]
+
+	def shade_scale(self, x, y):
+		return int(self.elevation_coordinates(x, y) - self.min_elevation) / (self.max_elevation - self.min_elevation) * 255)
+
+
+	def draw_map(self):
+		self.picture = Image.new('RBGA', (len(self.elevations[0], len(self.elevations)))
+		self.generate = ImageDraw.Draw(self.picture)
+			for x in range(len(self.elevations[0])):
+				for y in range(len(self.elevations)):
+		self.picture.putpixel((x, y),x self.shade_scale(x, y), self.shade_scale(x, y), self.shade_scale(x, y))
+		self.picture.save('map.png')
+
+if __name__ == '__main__':
+	elevation_map = Map('elevation_small.txt')
+	elevation_map.draw_map()
